04e361f339669b027f7fef3fa3a9654f1299b6fd
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2022-09-12 02:57:26 +0300

    Corrected detours

--- ninecraft/src/main.cpp
+++ ninecraft/src/main.cpp
@@ -58,11 +58,11 @@ std::string getCWD()
 void detour(void *dst_addr, void *src_addr, bool jump) {
     uint32_t page_size = sysconf(_SC_PAGESIZE);
     void *protect = (void *)((uintptr_t)(dst_addr) & -page_size);
-    mprotect(protect, page_size, PROT_READ | PROT_WRITE | PROT_EXEC);
+    mprotect(protect, 5, PROT_READ | PROT_WRITE | PROT_EXEC);
     uint32_t addr = ((uint32_t)src_addr) - ((uint32_t)dst_addr) - 5;
     *(uint8_t *)(dst_addr) = jump ? 0xE9 : 0xE8;
     *(uint32_t *)(((uint32_t)dst_addr) + 1) = addr;
-    mprotect(protect, page_size, PROT_EXEC);
+    mprotect(protect, 5, PROT_EXEC);
 }
 
 #else
@@ -70,33 +70,33 @@ void detour(void *dst_addr, void *src_addr, bool jump) {
 #ifdef __arm__
 
 uint32_t encode_branch(uint32_t pc, uint32_t branch_to, uint8_t has_link, uint8_t is_relative) {
-	uint32_t offset = branch_to - ((pc + 4) & 0xFFFFFFFE);
-	uint8_t s = (offset >> 24) & 1;
-	uint8_t j = (~((offset >> 23) & 1) ^ s) & 1;
-	uint8_t j2 = (~((offset >> 22) & 1) ^ s) & 1;
-	uint16_t h = (offset >> 12) & 0x3ff;
-	uint16_t l = (offset >> 1) & 0x7ff;
-	uint8_t type = has_link ? 0b11 : 0b10;
-	uint8_t thumb_bit = is_relative ? 1 : 0;
-	uint8_t opcode = 0b11110;
-	uint32_t result = opcode << 27;
-	result |= s << 26;
-	result |= h << 16;
-	result |= type << 14;
-	result |= j << 13;
-	result |= thumb_bit << 12;
-	result |= j2 << 11;
-	result |= l;
-	return ((result & 0xffff) << 16) | ((result >> 16) & 0xffff);
+    int32_t offset = branch_to - ((pc + 4) & 0xFFFFFFFC);
+    uint8_t s = (offset >> 31) & 1;
+    uint8_t j = (~((offset >> 23) & 1) ^ s) & 1;
+    uint8_t j2 = (~((offset >> 22) & 1) ^ s) & 1;
+    uint16_t h = (offset >> 12) & 0x3ff;
+    uint16_t l = (offset >> 1) & 0x7ff;
+    uint8_t type = has_link ? 0b11 : 0b10;
+    uint8_t thumb_bit = is_relative ? 1 : 0;
+    uint8_t opcode = 0b11110;
+    uint32_t result = opcode << 27;
+    result |= s << 26;
+    result |= h << 16;
+    result |= type << 14;
+    result |= j << 13;
+    result |= thumb_bit << 12;
+    result |= j2 << 11;
+    result |= l;
+    return ((result & 0xffff) << 16) | ((result >> 16) & 0xffff);
 }
 
-void detour(void *dst_addr, void *src_addr, bool jump) {
-	long page_size = sysconf(_SC_PAGESIZE);
-	void *protect = (void *)((uintptr_t)(dst_addr-1) & -page_size);
-	mprotect(protect, page_size, PROT_READ | PROT_WRITE | PROT_EXEC);
-	uint32_t b = encode_branch((int) dst_addr-1, (int) src_addr-1, jump ? 0 : 1, 1);
-	memcpy(dst_addr-1, (void *) &b, sizeof(uint32_t));
-	mprotect(protect, page_size, PROT_EXEC);
+void detour(void *dst_addr, void *src_addr, bool unused) {
+    long page_size = sysconf(_SC_PAGESIZE);
+    void *protect = (void *)(((uintptr_t)dst_addr - 1) & -page_size);
+    mprotect(protect, 4, PROT_READ | PROT_WRITE | PROT_EXEC);
+    uint32_t b = encode_branch((uint32_t)dst_addr-1, (uint32_t)src_addr-1, 0, 1);
+    *(uint32_t *)((uint32_t)dst_addr-1) = b;
+    mprotect(protect, 4, PROT_EXEC);
 }
 
 #else
