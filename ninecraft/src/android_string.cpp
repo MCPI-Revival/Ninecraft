d8123a656b5a1aee51f3d28aae0a2b62df0df1a6
Alexander Argentakis <113830694+TheFriedLemon@users.noreply.github.com>
2022-09-19 19:03:29 +0300

    used the internal string converter instead

--- ninecraft/src/android_string.cpp
+++ ninecraft/src/android_string.cpp
@@ -1,20 +1,29 @@
-#include <dlfcn.h>
-#include <iostream>
-#include <cstring>
-#include <fstream>
-#include <hybris/dlfcn.h>
 #include <hybris/hook.h>
-#include <hybris/jb/linker.h>
-#include <cstdlib>
 #include <ninecraft/android_string.hpp>
 
-char *to_str(void *str, char *cstr, void *handle) {
-    size_t length = strlen(cstr);
-    *(void **)(str + 16) = str;
-    *(void **)(str + 20) = str;
-    ((void (*)(void *, unsigned int)) hybris_dlsym(handle, "_ZNSt4priv12_String_baseIcSaIcEE17_M_allocate_blockEj"))(str, length + 1);
-    char *pcstr = *(char **)(str + 20);
-    memcpy(pcstr, cstr, length);
-    *(void **)(str + 16) = pcstr + length;
-    pcstr[length] = 0;
+void to_str(void *str, char *cstr, void *handle) {
+    #ifdef __i386__
+    void *p = hybris_dlsym(handle, "_ZN11BlankButtonD0Ev")+0x80;
+    asm volatile(
+        "mov %0, %%eax;"
+        "mov %1, %%edx;"
+        "call %2;"
+        :
+        : "r"(str), "r"(cstr), "r"(p)
+        : "%eax", "%edx"
+    );
+    #else
+    #ifdef __arm__
+    void *p = hybris_dlsym(handle, "_ZN6ButtonD0Ev")+0x12;
+    /*asm volatile(
+        "mov r0, %0;"
+        "mov r1, %1;"
+        "blx %2;"
+        :
+        : "r"(str), "r"(cstr), "r"(p)
+        : "r0", "r1"
+    );*/
+    ((void (*)(void *, const char *))p)(str, cstr);
+    #endif
+    #endif
 }
