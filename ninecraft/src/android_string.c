b13d731f83844896ffba225ba863d66baf8eb59b
MFDGaming <alexandros.argentakis@gmail.com>
2022-11-22 22:25:36 +0200

    fully moved to C

--- ninecraft/src/android_string.c
+++ ninecraft/src/android_string.c
@@ -1,31 +1,14 @@
 #include <hybris/dlfcn.h>
 #include <ninecraft/android_string.h>
+#include <string.h>
+#include <ninecraft/android_alloc.h>
 
 void to_str(android_string *str, char *cstr, void *handle) {
-    #ifdef __i386__
-    void *p = hybris_dlsym(handle, "_ZN11BlankButtonD0Ev")+0x80;
-    asm volatile(
-        "mov %0, %%eax;"
-        "mov %1, %%edx;"
-        "call %2;"
-        :
-        : "r"(str), "r"(cstr), "r"(p)
-        : "%eax", "%edx"
-    );
-    #else
-    #ifdef __arm__
-    void *p = hybris_dlsym(handle, "_ZN6ButtonD0Ev")+0x12;
-    /*asm volatile(
-        "mov r0, %0;"
-        "mov r1, %1;"
-        "blx %2;"
-        :
-        : "r"(str), "r"(cstr), "r"(p)
-        : "r0", "r1"
-    );*/
-    ((void (*)(void *, const char *))p)(str, cstr);
-    #endif
-    #endif
+    size_t size = strlen(cstr) + 1;
+    str->_M_start_of_storage = android_alloc$allocate((uint32_t *)&size, handle);
+    str->buffers._M_end_of_storage = str->_M_start_of_storage + size;
+    str->_M_finish = str->buffers._M_end_of_storage;
+    memcpy(str->_M_start_of_storage, cstr, size);
 }
 
 void android_string$string(android_string *__this, android_string *str, void *handle) {
