cee5627eaae49238e4aecc1424b924cb53f3504a
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2023-04-14 18:06:40 -0400

    Fix Running extract.sh With Space In Filename

--- tools/extract.sh
+++ tools/extract.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-if [ -z ${1} ]; then
+if [ -z "${1}" ]; then
     echo "Please specify the archive"
     exit
 fi
-if [ ! -f ${1} ]; then
+if [ ! -f "${1}" ]; then
     echo "The specified file doesn't exist!"
     exit
 fi
-if ! [[ ${1} =~ \.apk$ ]]; then
+if ! [[ "${1}" =~ \.apk$ ]]; then
     echo "The file must be an .apk"
     exit
 fi
 
 rm -rf assets/
 
-unzip -o ${1} "assets/*"
+unzip -o "${1}" "assets/*"
 
-unzip -o ${1} "res/*"
+unzip -o "${1}" "res/*"
 
-unzip -o ${1} "lib/*"
+unzip -o "${1}" "lib/*"
