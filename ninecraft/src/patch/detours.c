26e16632b345f7627191ad47d71bacb7118b75f4
MFDGaming <alexandros.argentakis@gmail.com>
2023-05-21 02:34:43 +0300

    Improved ARM detour

--- ninecraft/src/patch/detours.c
+++ ninecraft/src/patch/detours.c
@@ -3,15 +3,14 @@
 
 void arm_detour(void *target_addr, void *replacement_addr) {
     if (THUMB_TEST_BIT0((uint32_t)target_addr)) {
-        uint32_t start = 0;
+        *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr)) = 0xF8DF;
         if (THUMB_CLEAR_BIT0((uint32_t)target_addr) % 4 != 0) {
-            *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr)) = 0xBF00;
-            start = 2;
+            *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr) + 2) = 0xF002;
+        } else {
+            *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr) + 2) = 0xF000;
         }
-        *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr) + start) = 0xF8DF;
-        *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr) + start + 2) = 0xF000;
-        *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr) + start + 4) = (uint16_t)((uint32_t)replacement_addr & 0xFFFF);
-        *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr) + start + 6) = (uint16_t)((uint32_t)replacement_addr >> 16);
+        *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr) + 4) = (uint16_t)((uint32_t)replacement_addr & 0xFFFF);
+        *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr) + 6) = (uint16_t)((uint32_t)replacement_addr >> 16);
     } else {
         *(uint32_t *)target_addr = 0xe51ff004;
         *(uint32_t *)(target_addr + 4) = (uint32_t)replacement_addr;
