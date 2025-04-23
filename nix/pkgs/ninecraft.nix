279c2ec27a02e7639236bf2ee4e712e41618862c
Tumble <tumblegamer@gmail.com>
2025-04-23 21:42:08 +0100

    added more versions

--- nix/pkgs/ninecraft.nix
+++ nix/pkgs/ninecraft.nix
@@ -13,15 +13,14 @@
   curl,
   ninecraft-extract,
 }: let
-  ninecraft = stdenv.mkDerivation {
-    name = "ninecraft";
+  ninecraft-unpatched = stdenv.mkDerivation {
+    name = "ninecraft-unpatched";
     version = "1.2.0_56b9f26";
     src = ../..;
     nativeBuildInputs = [
       pkg-config
       cmake
     ];
-    patches = [./use-system-dependancies.patch];
     buildInputs = [
       xorg.libX11
       xorg.libXrandr
@@ -41,49 +40,53 @@
       cp -r $src/internal_overrides $out/var/lib/ninecraft
     '';
   };
-  entryScript = writeScript "ninecraft" ''
-    export NINECRAFT_DATA=''${XDG_DATA_HOME:-$HOME/.local/share}/ninecraft
-    if [[ ! -d "$NINECRAFT_DATA" ]]; then
-    echo "Setting up data folder at: $NINECRAFT_DATA"
-      mkdir -p "$NINECRAFT_DATA"
-    fi
-
-    cd $NINECRAFT_DATA
-
-    if [[ ! -d "internal_overrides" ]]; then
-      echo "copying internal_overrides..."
-      cp -r ${ninecraft}/var/lib/ninecraft/internal_overrides .
-    fi
-
-
-
-    export VERSION=$1
-    export ARCH=$2
-
-    if [[ -z "$VERSION" ]]; then
-      if [[ ! -d "lib" ]]; then
-        VERSION=0.5.0
+  ninecraft = ninecraft-unpatched.overrideAttrs {
+    patches = [./use-system-dependancies.patch];
+  };
+  entryScript = ninecraft:
+    writeScript "ninecraft" ''
+      export NINECRAFT_DATA=''${XDG_DATA_HOME:-$HOME/.local/share}/ninecraft
+      if [[ ! -d "$NINECRAFT_DATA" ]]; then
+      echo "Setting up data folder at: $NINECRAFT_DATA"
+        mkdir -p "$NINECRAFT_DATA"
       fi
-    fi
 
-    if [[ -n "$VERSION" ]]; then
-      if [[ -e "mcpe.apk" ]]; then
-        rm mcpe.apk
+      cd $NINECRAFT_DATA
+
+      if [[ ! -d "internal_overrides" ]]; then
+        echo "copying internal_overrides..."
+        cp -r ${ninecraft}/var/lib/ninecraft/internal_overrides .
       fi
-      rm -rf assets res lib
-      export URL="https://archive.org/download/MCPEAlpha/PE-a''${VERSION}-''${ARCH:-x86}.apk"
-      echo "Downloading Minecraft PE $VERSION from $URL..."
-      ${curl}/bin/curl -L --output mcpe.apk "$URL"
 
-      echo "Extracting APK..."
-      ${ninecraft-extract}/bin/ninecraft-extract mcpe.apk
 
-    fi
-    pwd
-    ls
-    echo "Starting Ninecraft..."
-    ${ninecraft}/bin/ninecraft
-  '';
+
+      export VERSION=$1
+      export ARCH=$2
+
+      if [[ -z "$VERSION" ]]; then
+        if [[ ! -d "lib" ]]; then
+          VERSION=0.5.0
+        fi
+      fi
+
+      if [[ -n "$VERSION" ]]; then
+        if [[ -e "mcpe.apk" ]]; then
+          rm mcpe.apk
+        fi
+        rm -rf assets res lib
+        export URL="https://archive.org/download/MCPEAlpha/PE-a''${VERSION}-''${ARCH:-x86}.apk"
+        echo "Downloading Minecraft PE $VERSION from $URL..."
+        ${curl}/bin/curl -L --output mcpe.apk "$URL"
+
+        echo "Extracting APK..."
+        ${ninecraft-extract}/bin/ninecraft-extract mcpe.apk
+
+      fi
+      pwd
+      ls
+      echo "Starting Ninecraft..."
+      ${ninecraft}/bin/ninecraft
+    '';
 in
   stdenv.mkDerivation {
     name = "ninecraft";
@@ -91,7 +94,10 @@ in
     phases = ["buildPhase"];
     buildPhase = ''
       mkdir -p $out/bin
-      cp ${entryScript} $out/bin/ninecraft
+      cp ${entryScript ninecraft} $out/bin/ninecraft
+      cp ${ninecraft}/bin/ninecraft $out/bin/ninecraft-unwrapped
+      cp ${ninecraft-unpatched}/bin/ninecraft $out/bin/ninecraft-unpatched
+      cp ${entryScript ninecraft} $out/bin/ninecraft-wrapped-unpatched
       cp ${ninecraft-extract}/bin/ninecraft-extract $out/bin
     '';
 
