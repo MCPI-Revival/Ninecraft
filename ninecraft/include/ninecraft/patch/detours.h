fb738f866b7399a230159ec8b79727fd5a8acf9e
MFDGaming <alexandros.argentakis@gmail.com>
2025-05-11 17:46:25 +0300

    fixed the seed in zenity dialogs & added trampolines in detours

--- ninecraft/include/ninecraft/patch/detours.h
+++ ninecraft/include/ninecraft/patch/detours.h
@@ -3,13 +3,9 @@
 
 #include <stdbool.h>
 
-#define THUMB_SET_BIT0(addr) (addr | 0x01)
-#define THUMB_CLEAR_BIT0(addr) (addr & 0xfffffffe)
-#define THUMB_TEST_BIT0(addr) (addr & 0x01)
+extern void *arm_detour(void *target_addr, void *replacement_addr);
 
-extern void arm_detour(void *target_addr, void *replacement_addr);
-
-extern void x86_detour(void *target_addr, void *replacement_addr, bool jump);
+extern void *x86_detour(void *target_addr, void *replacement_addr, bool jump);
 
 #if defined(__i386__) || defined(_M_IX86)
 #define DETOUR(target_addr, replacement_addr, jump) (x86_detour(target_addr, replacement_addr, jump))
--- ninecraft/src/patch/detours.c
+++ ninecraft/src/patch/detours.c
@@ -1,25 +1,72 @@
 #include <ninecraft/patch/detours.h>
 #include <stdint.h>
 #include <string.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/mman.h>
+#endif
 
-void arm_detour(void *target_addr, void *replacement_addr) {
-    if (THUMB_TEST_BIT0((uint32_t)target_addr)) {
-        *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr)) = 0xF8DF;
-        if (THUMB_CLEAR_BIT0((uint32_t)target_addr) % 4 != 0) {
-            *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr) + 2) = 0xF002;
+static uintptr_t trampoline_alloc() {
+#ifdef _WIN32
+    return (uintptr_t)VirtualAlloc(NULL, 0x1000, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+#else
+    return (uintptr_t)mmap(NULL, 0x1000, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+#endif
+}
+
+static uintptr_t arm_trampoline(uintptr_t target_addr) {
+    uintptr_t trampoline = trampoline_alloc();
+    trampoline = (trampoline + 3) & ~3;
+
+    if (target_addr & 1) {
+        uintptr_t addr = target_addr & ~1;
+        *(uint32_t *)trampoline = *(uint32_t *)addr;
+        *(uint32_t *)(trampoline + 4) = *(uint32_t *)(addr + 4);
+        *(uint32_t *)(trampoline + 8) = 0xF000F8DF;
+        *(uint32_t *)(trampoline + 12) = target_addr + 8;
+        return trampoline + 1;
+    }
+    *(uint32_t *)trampoline = *(uint32_t *)target_addr;
+    *(uint32_t *)(trampoline + 4) = *(uint32_t *)(target_addr + 4);
+    *(uint32_t *)(trampoline + 8) = 0xe51ff004;
+    *(uint32_t *)(trampoline + 12) = target_addr + 8;
+    return trampoline;
+}
+
+static uintptr_t x86_trampoline(uintptr_t target_addr) {
+    uintptr_t trampoline = trampoline_alloc();
+    trampoline = (trampoline + 3) & ~3;
+    memcpy((void *)trampoline, (void *)target_addr, 5);
+    *(uint8_t *)(target_addr + 5) = 0xe9;
+    *(uint32_t *)(target_addr + 6) = trampoline - target_addr;
+    return trampoline;
+}
+
+void *arm_detour(void *target_addr, void *replacement_addr) {
+    void *trampoline = (void *)arm_trampoline((uintptr_t)target_addr);
+    if ((uintptr_t)target_addr & 1) {
+        uintptr_t addr = (uintptr_t)target_addr & ~1;
+        *(uint16_t *)addr = 0xF8DF;
+        if (addr % 4 != 0) {
+            *(uint16_t *)(addr + 2) = 0xF002;
         } else {
-            *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr) + 2) = 0xF000;
+            *(uint16_t *)(addr + 2) = 0xF000;
         }
-        *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr) + 4) = (uint16_t)((uint32_t)replacement_addr & 0xFFFF);
-        *(uint16_t *)(THUMB_CLEAR_BIT0((uint32_t)target_addr) + 6) = (uint16_t)((uint32_t)replacement_addr >> 16);
+        *(uint32_t *)(addr + 4) = (uintptr_t)replacement_addr;
     } else {
         *(uint32_t *)target_addr = 0xe51ff004;
         *(uint32_t *)((char *)target_addr + 4) = (uint32_t)replacement_addr;
     }
+    return trampoline;
 }
 
-void x86_detour(void *target_addr, void *replacement_addr, bool jump) {
-    uint32_t addr = (uint32_t)replacement_addr - (uint32_t)target_addr - 5;
-    ((uint8_t *)target_addr)[0] = jump ? 0xe9 : 0xe8;
-    memcpy(&((uint8_t *)target_addr)[1], &addr, 4);
+void *x86_detour(void *target_addr, void *replacement_addr, bool jump) {
+    void *trampoline = NULL;
+    if (jump) {
+        trampoline = (void *)x86_trampoline((uintptr_t)target_addr);
+    }
+    *(uint8_t *)target_addr = jump ? 0xe9 : 0xe8;
+    *(uint32_t *)((uintptr_t)target_addr + 1) = (uintptr_t)replacement_addr - (uintptr_t)target_addr - 5;
+    return trampoline;
 }
