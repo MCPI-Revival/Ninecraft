465d48dc3114f38f98d965a69a7ea32a6f063f30
MFDGaming <alexandros.argentakis@gmail.com>
2025-05-19 19:24:04 +0300

    fixed boat controlling in 0.11.0

--- ninecraft/src/mods/inputs_fix_mod.c
+++ ninecraft/src/mods/inputs_fix_mod.c
@@ -81,6 +81,38 @@ typedef struct {
     void *options;
 } keyboard_input_0_11_0_t;
 
+typedef struct {
+    void **vtable;
+    float h_pos;
+    float v_pos;
+    bool unknown0;
+    bool is_flying_up;
+    bool is_flying_down;
+    bool unknown5;
+    bool is_jumping;
+    bool unknown6;
+    bool forward_pressed;
+    bool back_pressed;
+    bool left_pressed;
+    bool right_pressed;
+    bool jump_pressed;
+    bool sneak_pressed;
+    bool crafting_pressed;
+    bool unknown13;
+    bool unknown14;
+    bool unknown15;
+    void *options;
+    void *unknown16;
+    void *boat;
+    bool unknown40;
+    bool paddle_right;
+    bool paddle_left;
+    bool side_right;
+    bool old_paddle_right;
+    bool old_paddle_left;
+    bool side_left;
+} boat_input_0_11_0_t;
+
 typedef union {
     keyboard_input_0_11_0_t v_0_11_0;
     keyboard_input_0_6_0_t v_0_6_0;
@@ -133,6 +165,19 @@ void xperia_play_input_set_key(keyboard_input_t *__this, int id, bool state) {
     ((void (*)(keyboard_input_t *, int, bool))android_dlsym(_handle, "_ZN13KeyboardInput6setKeyEib"))(__this, id, state);
 }
 
+void boat_input_tick(boat_input_0_11_0_t *__this, void *player) {
+    ((void (*)(boat_input_0_11_0_t *, void *))android_dlsym(_handle, "_ZN13KeyboardInput4tickER6Player"))(__this, player);
+    __this->paddle_right = __this->right_pressed || __this->forward_pressed;
+    __this->paddle_left = __this->left_pressed || __this->forward_pressed;
+    __this->side_right = ((bool (*)(void *, int, bool, bool))android_dlsym(_handle, "_ZN4Boat14setPaddleStateE4Sidebb"))(__this->boat, 0, __this->paddle_right, __this->old_paddle_right);
+    __this->side_left = ((bool (*)(void *, int, bool, bool))android_dlsym(_handle, "_ZN4Boat14setPaddleStateE4Sidebb"))(__this->boat, 1, __this->paddle_left, __this->old_paddle_left);
+    __this->old_paddle_right = __this->paddle_right;
+    __this->old_paddle_left = __this->paddle_left;
+    __this->paddle_right = 0;
+    __this->paddle_left = 0;
+    __this->unknown5 = 0;
+}
+
 void inputs_fix_mod_inject(void *handle, int version_id) {
     _handle = handle;
     _version_id = version_id;
@@ -140,6 +185,10 @@ void inputs_fix_mod_inject(void *handle, int version_id) {
         ((void **)android_dlsym(handle, "_ZTV15XperiaPlayInput"))[4] = xperia_play_input_tick;
     } else if (version_id >= version_id_0_9_0 && version_id <= version_id_0_11_0) {
         ((void **)android_dlsym(handle, "_ZTV19ControllerMoveInput"))[4] = xperia_play_input_tick;
-         ((void **)android_dlsym(handle, "_ZTV19ControllerMoveInput"))[6] = xperia_play_input_set_key;
+        ((void **)android_dlsym(handle, "_ZTV19ControllerMoveInput"))[6] = xperia_play_input_set_key;
+    }
+    if (version_id >= version_id_0_11_0) {
+        ((void **)android_dlsym(handle, "_ZTV23BoatControllerMoveInput"))[4] = boat_input_tick;
+        ((void **)android_dlsym(handle, "_ZTV23BoatControllerMoveInput"))[6] = xperia_play_input_set_key;
     }
 }
