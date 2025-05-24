46e32e3c5974471358a493a167e1fbbfa3ebd1d0
Skjeggegubben <145590798+Skjeggegubben@users.noreply.github.com>
2025-05-24 14:45:04 +0200

    Update mod_loader.h
    Give mods ability to fire functions upon keys being pressed or released

--- ninecraft/include/ninecraft/mods/mod_loader.h
+++ ninecraft/include/ninecraft/mods/mod_loader.h
@@ -11,6 +11,9 @@ extern void mod_loader_execute_on_minecraft_grab_mouse(void *minecraft, int vers
 
 extern void mod_loader_execute_on_minecraft_release_mouse(void *minecraft, int version_id);
 
+// Give mods ability to fire functions upon keys being pressed or released
+extern void mod_loader_execute_on_key(int key, int action);
+
 extern void mod_loader_load_all(void *minecraft_handle, int version_id);
 
 #endif
