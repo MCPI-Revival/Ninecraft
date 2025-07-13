7d4ec4fe316637269272b622ddeb51019667194b
Tumble <tumblegamer@gmail.com>
2025-07-13 10:45:27 +0100

    descriptions and stuff

--- nix/pkgs/desktop.nix
+++ nix/pkgs/desktop.nix
@@ -8,7 +8,10 @@
 makeDesktopItem {
   desktopName = "Ninecraft";
   name = "ninecraft";
+  GenericName = "MCPE Alpha Player";
+  Comment = "A mcpe 0.1 .0 - 0.10 .5 launcher for linux and windows"
   inherit icon;
   exec = "ninecraft --home \"${homeDir}\" --game \"${gameDir}\"";
   categories = ["Game" "AdventureGame"];
+  Keywords = ["mcpe""android""mcpi""pocket""edition"];
 }
