98b1d711e545e87127f01b9f4aeab467d2f57d3c
Tumble <tumblegamer@gmail.com>
2025-07-16 11:52:34 +0100

    build deb file

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,13 +1,16 @@
-{
-  mkShell,
-  ninecraft,
-  ninecraft-extract,
-  ninecraft-nixgl,
-}:
-mkShell {
-  packages = [
-    ninecraft
-    ninecraft-extract
-    ninecraft-nixgl
-  ];
-}
+# compilation shell
+with import <nixpkgs> {};
+  mkShell {
+    nativeBuildInputs = [
+      pkg-config
+      cmake
+    ];
+    buildInputs = [
+      python312Packages.jinja2
+      zlib
+      SDL2
+    ];
+    packages = [
+      make
+    ];
+  }
