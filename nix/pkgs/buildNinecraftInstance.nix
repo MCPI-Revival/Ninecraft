5e7104c6654f984ad9356fd4cad1911d85bbda5c
Tumble <tumblegamer@gmail.com>
2025-08-07 18:11:00 +0100

    added version installer

--- nix/pkgs/buildNinecraftInstance.nix
+++ nix/pkgs/buildNinecraftInstance.nix
@@ -54,66 +54,29 @@
       // options
     )
   );
+  ninecraftLib =
+    writeShellScript "ninecraft-lib" (builtins.readFile ./ninecraft-lib.sh);
   optionsTxt = writeText "options.txt" optionsTxtContent;
   startScript =
     writeShellScript "start-ninecraft-${name}.sh" ''
+      source ${ninecraftLib}
       homeDir=${homeDir}
       ${
         lib.optionalString (gameDir != null) ''
           gameDir=${gameDir}
 
-          # Version
-          if [ ! -d $gameDir ]; then
-            mkdir -p $gameDir
-          fi
-          if [[ ( ! -d "$gameDir/lib"  ) || ( ! -d "$gameDir/res"  ) || ( ! -d "$gameDir/assets"  ) ]]; then
-            cp -r "${version}"/{assets,lib,res} "$gameDir"
-            chmod -R u+w "$gameDir"/{assets,lib,res}
-          fi
-        ''
-      }
-      ${
-        lib.optionalString versionParam ''
-          # Version Param
-          if [ -z "$gameDir" ]; then
-            gameDir=''${homeDir:-''${XDG_DATA_HOME:-$HOME/.local/share}/ninecraft}
-          fi
-
-          if [[ ! -d "$gameDir" ]]; then
-          echo "Setting up data folder at: $gameDir"
-            mkdir -p "$gameDir"
-          fi
-
-          cd $gameDir
-          export VERSION=$1
-          export ARCH=$2
-
-          if [[ -z "$VERSION" ]]; then
-            if [[ ! -d "lib" ]]; then
-              VERSION=0.5.0
-            fi
-          fi
-
-          if [[ -n "$VERSION" ]]; then
-            if [[ -e "mcpe.apk" ]]; then
-              rm mcpe.apk
-            fi
-            rm -rf assets res lib
-            export URL="https://archive.org/download/MCPEAlpha/PE-a''${VERSION}-''${ARCH:-${
+          # Install Version
+          if [ -z "$1" ]; then
+            installVersion "${version}"
+          else
+            installVersion "$1" "${
             if stdenvNoCC
 .hostPlatform.isx86
             then "x86"
             else ""
-          }}.apk"
-            echo "Downloading Minecraft PE $VERSION from $URL..."
-            ${curl}/bin/curl -L --output mcpe.apk "$URL"
-
-            echo "Extracting APK..."
-            ${ninecraft-extract} mcpe.apk
-
+          }"
           fi
-          pwd
-          ls
+
         ''
       }
       ${lib.optionalString (builtins.isAttrs options) ''
@@ -158,7 +121,10 @@ in
       runHook postInstall
     '';
 
-    desktopItems = makeNinecraftDesktopItems {
-      instance = name;
-    };
+    desktopItems = lib.optional true (builtins.elemAt (
+        makeNinecraftDesktopItems {
+          instance = name;
+        }
+      )
+      0);
   }
